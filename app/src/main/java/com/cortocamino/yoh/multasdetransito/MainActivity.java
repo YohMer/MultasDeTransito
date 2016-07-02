97a9593f28d3801b298642b2c33652296d39f957
yoh <yoh_m@yahoo.fr>
2016-07-02 18:26:22 -0400

    save cedula number

--- app/src/main/java/com/cortocamino/yoh/multasdetransito/MainActivity.java
+++ app/src/main/java/com/cortocamino/yoh/multasdetransito/MainActivity.java
@@ -1,7 +1,11 @@
 package com.cortocamino.yoh.multasdetransito;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int defaultCedulaNb = Integer.parseInt(getString(R.string.default_cedula_nb));
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        int cedulaNb = sharedPref.getInt("cedula_nb", defaultCedulaNb);
+
+        EditText cedulaEditText = (EditText) findViewById(R.id.cedulaNb);
+        cedulaEditText.setText("" + cedulaNb);
+    }
+
+    public void saveCedula(View view){
+        EditText cedulaEditText = (EditText) findViewById(R.id.cedulaNb);
+        int cedulaNb = Integer.parseInt(cedulaEditText.getText().toString());
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putInt("cedula_nb" ,cedulaNb);
+        editor.commit();
     }
 }
